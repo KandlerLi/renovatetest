15577b16fad9f0d2f6b12c255b0a5d4f1d29506a
Julian Kandler <julian.kandler@outlook.com>
2022-08-08 10:45:36 +0200

    first commit

---
+++ renovatetest/src/main/kotlin/li/kandler/renovatetest/RenovatetestApplication.kt
@@ -0,0 +1,11 @@
+package li.kandler.renovatetest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RenovatetestApplication
+
+fun main(args: Array<String>) {
+	runApplication<RenovatetestApplication>(*args)
+}
---
+++ renovatetest/src/test/kotlin/li/kandler/renovatetest/RenovatetestApplicationTests.kt
@@ -0,0 +1,13 @@
+package li.kandler.renovatetest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RenovatetestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
